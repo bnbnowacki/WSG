050c068819b0ff30c05188fc388d5761f0670657
Bartosz Nowacki <bnbnowacki@gmail.com>
2017-10-19 01:00:26 +0200

    test

--- WypozyczalniaSprzetuWSG/WypozyczalniaSprzetuWSG/Form1.Designer.cs
+++ WypozyczalniaSprzetuWSG/WypozyczalniaSprzetuWSG/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Freestyle Script", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(59, 201);
+            this.label1.Location = new System.Drawing.Point(50, 134);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(1244, 112);
             this.label1.TabIndex = 0;
